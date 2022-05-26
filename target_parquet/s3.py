12892772c4e18190d3223f226d8dc22c1dd754b0
Yash <yash.dabhi@marutitech.com>
2022-05-26 09:53:30 +0530

    Update

--- target_parquet/s3.py
+++ target_parquet/s3.py
@@ -25,10 +25,10 @@ def create_client(config):
     LOGGER.info("Attempting to create AWS session")
 
     # Get the required parameters from config file and/or environment variables
-    aws_access_key_id = config.get('aws_access_key_id') or os.environ.get('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key = config.get('aws_secret_access_key') or os.environ.get('AWS_SECRET_ACCESS_KEY')
-    aws_session_token = config.get('aws_session_token', os.environ.get('AWS_SESSION_TOKEN'))
-    aws_profile = config.get('aws_profile') or os.environ.get('AWS_PROFILE')
+    aws_access_key_id = config.get('aws_access_key_id')
+    aws_secret_access_key = config.get('aws_secret_access_key')
+    aws_session_token = config.get('aws_session_token')
+    aws_profile = config.get('aws_profile')
     aws_endpoint_url = config.get('aws_endpoint_url')
 
     # AWS credentials based authentication
